07dcaaaf390079d2984947ecbfa996e00b6c2a7b
Matpdev <matheus2ep@gmail.com>
2023-08-22 14:49:34 -0300

    First Commit

---
+++ OutBank/Program.cs
@@ -0,0 +1,13 @@
+﻿using System.IO;
+
+namespace Start
+{
+    class StartSystem
+    {
+        private static void Main()
+        {
+            Console.WriteLine("");
+            return;
+        }
+    }
+}
